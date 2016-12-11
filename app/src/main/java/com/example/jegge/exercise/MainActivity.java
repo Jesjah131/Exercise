f61aef86b2415e551bc84d0b77b40ce91fb1cc1c
Jesper Jansson¨ <jesperjansson@live.se>
2016-12-11 18:48:54 +0100

    bakgrundsbild

--- app/src/main/java/com/example/jegge/exercise/MainActivity.java
+++ app/src/main/java/com/example/jegge/exercise/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         Button goToManageWorkoutAndExercies = (Button) findViewById(R.id.ButtonForManageWorkoutAndExercies);
-        goToSession.setOnClickListener(new View.OnClickListener(){
+        goToManageWorkoutAndExercies.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View view){
